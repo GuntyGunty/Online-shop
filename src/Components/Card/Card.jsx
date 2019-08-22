64a90b91caeda64295ee01b9abca1398b2ee532a
GuntyGunty <gunter0705@yandex.ru>
2019-08-22 22:26:20 +0300

    .

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,11 +4,14 @@ import './Card.css'
 const Card = ({
   children,
   cardLink,
+  imgPath,
 }) => {
   return (
     <div className="Card">
       <a href={cardLink} className="Card-link">
-        <div className="Card-top"></div>
+        <div className="Card-top">
+          <img className="Card-img" src={imgPath} />
+        </div>
         <div className="Card-bottom"></div>
       </a>
     </div>
