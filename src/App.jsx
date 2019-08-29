5d76661c44bb47fac2b9864c8a9ae87aca4f1d9f
Makar Glavanar <mr.glavanar@gmail.com>
2019-08-30 02:44:16 +0900

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import APISerice from './Utils/APIService';
 
 import Container from './Components/Container/Container';
@@ -9,81 +10,24 @@ import Logo from './Components/Logo/Logo';
 import Card from './Components/Card/Card';
 import Filter from './Components/Filter/Filter';
 
+import HomePage from './Pages/Home/HomePage';
+import CataloguePage from './Pages/Catalogue/CataloguePage';
+
 export default class App extends React.Component {
-	
-	onFormSubmit = (e) => {
-		e.preventDefault();
-
-		const formData = new FormData(e.target);
-
-		const email = formData.get('email');
-		const password = formData.get('password');
-
-		const headers = new Headers();
-		headers.append('Content-Type', 'application/json');
-
-		fetch('https://edu-online-shop.herokuapp.com/api/v1/user/addOne', {
-			headers,
-			method: 'POST',
-			cache: 'no-cache',
-			mode: 'cors',
-			body: JSON.stringify({ email, password }),
-		}).then((response) => {
-			response.json().then((data) => {
-				if (!data.isError) {
-					localStorage.setItem('token', data.token);
-					localStorage.setItem('user', data.user);
-				}
-			});
-		});
-	}
-
-	componentDidMount() {
-		APISerice.post({
-			url: 'user/login',
-			body: {
-				email: 'mr.glavanar@bk.ru',
-				password: '12456',
-			},
-		}).then((data) => {
-			console.log(data);
-		});
-	}
 
 	render() {
 		return (
 			<div className="App">
-				{/* <form onSubmit={this.onFormSubmit}>
-					<label>
-						Email
-					<input name="email" type="email" />
-					</label>
-					<label>
-						Password
-					<input name="password" type="password" />
-					</label>
-					<input type="submit" />
-				</form> */}
-				<Container>
-					<Header>
-						<Logo></Logo>
-					</Header>
-				</Container>
-				<Container variant="flex">
-					<Sidebar>
-
-					</Sidebar>
-					<Main>
-						<Filter>
-			
-						</Filter>
-						<Container variant="inner flex">
-							<Card></Card>
-							<Card></Card>
-							<Card></Card>
-						</Container>
-					</Main>
-				</Container>
+				<Header>
+					<Link to="/catalogue">Catalogue</Link>
+					<Link to="/">Home</Link>
+				</Header>
+				<Route path="/" component={() => { window.scrollTo(0, 0); return (<></>); }} />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/catalogue" component={CataloguePage} />
+					<Route component={() => <h1>Not found!</h1>} />
+				</Switch>
 			</div>
 		);
 	}
---
+++ src/Pages/Catalogue/CataloguePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class CataloguePage extends React.Component {
+  render() {
+    return (
+      <div className="Catalogue">Catalogue!</div>
+    );
+  }
+}
